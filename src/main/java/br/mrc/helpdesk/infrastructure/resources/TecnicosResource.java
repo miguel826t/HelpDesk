ea4944034ac7b6ea105c41558c4eb38c3c7b1228
miguel826t <miguel826t@gmail.com>
2022-12-15 09:17:42 -0300

    EndPoint findAll para tecnico

--- src/main/java/br/mrc/helpdesk/infrastructure/resources/TecnicosResource.java
+++ src/main/java/br/mrc/helpdesk/infrastructure/resources/TecnicosResource.java
@@ -1,5 +1,8 @@
 package br.mrc.helpdesk.infrastructure.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,4 +29,13 @@ public class TecnicosResource {
 		Tecnico tecnico = tecnicoService.findById(id);
 		return ResponseEntity.ok().body(new TecnicoDTO(tecnico));
 	}
+	
+	@GetMapping
+	public ResponseEntity<List<TecnicoDTO>> findAll(){
+		List<Tecnico> list = tecnicoService.findAll();
+		
+		List<TecnicoDTO> listDTO = list.stream().map(t -> new TecnicoDTO(t)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+	
 }
--- src/main/java/br/mrc/helpdesk/infrastructure/services/TecnicoService.java
+++ src/main/java/br/mrc/helpdesk/infrastructure/services/TecnicoService.java
@@ -1,13 +1,14 @@
 package br.mrc.helpdesk.infrastructure.services;
 
+import java.util.List;
 import java.util.Optional;
 
-import br.mrc.helpdesk.infrastructure.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.mrc.helpdesk.domain.entities.Tecnico;
 import br.mrc.helpdesk.domain.repositories.TecnicoRepository;
+import br.mrc.helpdesk.infrastructure.services.exceptions.ObjectNotFoundException;
 /**
  * Service do Tecnico.
  * AULA: 11
@@ -23,4 +24,7 @@ public class TecnicoService {
 		return obj.orElseThrow(()-> new ObjectNotFoundException("Tecnico não encontrado! Id: "+id));
 	}
 	
+	public List<Tecnico> findAll(){
+		return tecnicos.findAll();
+	}
 }
